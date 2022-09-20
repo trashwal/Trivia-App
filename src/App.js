3ec327d09b31316338b620d7e0eb83445a5eb693
Youssef Ahmed <youssefashwal@outlook.com>
2022-09-20 05:33:33 +0200

    prettified file

--- src/App.js
+++ src/App.js
@@ -1,94 +1,98 @@
-import React from "react";
-import "./App.css";
-import Start from "./components/start";
-import Questions from "./components/questions";
+import React from 'react';
+import './App.css';
+import Start from './components/start';
+import Questions from './components/questions';
 
 export default function App() {
-
-    const [started, setStarted] = React.useState(false)
-    const [flip, setFlip] = React.useState(false)
-    const [loading, setLoading] = React.useState(false)
-    const [triviaData, setTriviaData] = React.useState([])
-    const [answers, setAnswers] = React.useState()
+    const [started, setStarted] = React.useState(false);
+    const [flip, setFlip] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+    const [triviaData, setTriviaData] = React.useState([]);
+    const [answers, setAnswers] = React.useState();
     const [userAnswers, setUserAnswers] = React.useState({
-        question1: "",
-        question2: "",
-        question3: "",
-        question4: "",
-        question5: ""
-    })
-    const [quizFinished, setQuizFinished] = React.useState(false)
+        question1: '',
+        question2: '',
+        question3: '',
+        question4: '',
+        question5: '',
+    });
+    const [quizFinished, setQuizFinished] = React.useState(false);
 
     function toggleQuiz() {
-        setStarted(true)
-        setFlip(prev => !prev)
-        setLoading(true)
-        setQuizFinished(false)
+        setStarted(true);
+        setFlip((prev) => !prev);
+        setLoading(true);
+        setQuizFinished(false);
         setUserAnswers({
-            question1: "",
-            question2: "",
-            question3: "",
-            question4: "",
-            question5: ""
-        })
+        question1: '',
+        question2: '',
+        question3: '',
+        question4: '',
+        question5: '',
+        });
     }
-    
-    React.useEffect(() => {
-        fetch("https://opentdb.com/api.php?amount=5&type=multiple&encode=base64")
-        .then(res => res.json())
-        .then(data => {
-            setTriviaData(data.results)
-            setLoading(false)
-        })
-    }, [flip])
 
     React.useEffect(() => {
-        setAnswers(triviaData.map(item => {
-            return (shuffle([item.correct_answer, ...item.incorrect_answers]))
-        }))
-    }, [triviaData])
-    
+        fetch('https://opentdb.com/api.php?amount=5&type=multiple&encode=base64')
+        .then((res) => res.json())
+        .then((data) => {
+            setTriviaData(data.results);
+            setLoading(false);
+        });
+    }, [flip]);
+
+    React.useEffect(() => {
+        setAnswers(
+        triviaData.map((item) => {
+            return shuffle([item.correct_answer, ...item.incorrect_answers]);
+        })
+        );
+    }, [triviaData]);
+
     const questionsData = triviaData.map((item, index) => {
         return {
-            question: atob(item.question),
-            correctAnswer: atob(item.correct_answer),
-            allAnswers: answers[index]
-        }
-    })
+        question: atob(item.question),
+        correctAnswer: atob(item.correct_answer),
+        allAnswers: answers[index],
+        };
+    });
 
     function shuffle(array) {
         for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        } return (array)
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
     }
-    
+
     function handleClick(event) {
-        const {name, id} = event.target
-        quizFinished ? setUserAnswers(prev => prev) :
-        setUserAnswers(prev => {
+        const { name, id } = event.target;
+        quizFinished
+        ? setUserAnswers((prev) => prev)
+        : setUserAnswers((prev) => {
             return {
                 ...prev,
-                [name]: id
-            }
-        })
+                [name]: id,
+            };
+            });
     }
 
     return (
         <main>
-            {!started ? 
-                <Start 
-                    startQuiz={toggleQuiz} /> :
-                loading ? 
-                    <div className="loading"> Loading... </div> :
-                    <Questions 
-                        data={questionsData} 
-                        userAnswers={userAnswers}
-                        handleClick={handleClick}
-                        quizFinished={quizFinished}
-                        setQuizFinished={setQuizFinished}
-                        restartQuiz={toggleQuiz}/>
-            }
+        {!started ? (
+            <Start startQuiz={toggleQuiz} />
+        ) : loading ? (
+            <div className="loading"> Loading... </div>
+        ) : (
+            <Questions
+            data={questionsData}
+            userAnswers={userAnswers}
+            handleClick={handleClick}
+            quizFinished={quizFinished}
+            setQuizFinished={setQuizFinished}
+            restartQuiz={toggleQuiz}
+            />
+        )}
         </main>
-    )
+    );
 }
--- src/components/questions.js
+++ src/components/questions.js
@@ -1,72 +1,83 @@
-import React from "react";
-import { nanoid } from "nanoid";
+import React from 'react';
+import { nanoid } from 'nanoid';
 
 export default function Questions(props) {
-        
     function checkAnswers() {
-        Object.values(props.userAnswers).includes("") ?
-        alert("Try to answer all the questions before checking the right ones.") :
-        props.setQuizFinished(true)
+        Object.values(props.userAnswers).includes('')
+        ? alert('Try to answer all the questions before checking the right ones.')
+        : props.setQuizFinished(true);
     }
 
     const questionComponent = props.data.map((item, index) => {
-        const question = <h2 className="questions--question">{index + 1}. {item.question}</h2>
+        const question = (
+        <h2 className="questions--question">
+            {index + 1}. {item.question}
+        </h2>
+        );
 
-        const answers = item.allAnswers.map(answer => {
-            const answerDecoded = atob(answer)
-            const name = `question${index + 1}`
-            let className = "questions--answer"
-            if (props.quizFinished) {
-                if (answerDecoded === item.correctAnswer) {
-                    className = "questions--answer correct"
-                } else if (answerDecoded === props.userAnswers[name]) {
-                    className = "questions--answer wrong"
-                }
+        const answers = item.allAnswers.map((answer) => {
+        const answerDecoded = atob(answer);
+        const name = `question${index + 1}`;
+        let className = 'questions--answer';
+        if (props.quizFinished) {
+            if (answerDecoded === item.correctAnswer) {
+            className = 'questions--answer correct';
             } else if (answerDecoded === props.userAnswers[name]) {
-                className = "questions--answer picked"
+            className = 'questions--answer wrong';
             }
-
-            return (
-            <label key={nanoid()} className={className} htmlFor={answerDecoded}>
-                {answerDecoded}
-                <input
-                    type="radio"
-                    id={answerDecoded}
-                    name={name}
-                    onClick={props.handleClick}
-                />
-            </label>)
-        })
+        } else if (answerDecoded === props.userAnswers[name]) {
+            className = 'questions--answer picked';
+        }
 
         return (
-            <div key={nanoid()} className="questions--questionComponent">
-                {question}
-                <div className="questions--answerContainer">
-                    {answers}
-                </div>
-            </div>
-        )
-    })
-    
-    const correctAnswers = props.data.map(item => item.correctAnswer)
+            <label key={nanoid()} className={className} htmlFor={answerDecoded}>
+            {answerDecoded}
+            <input
+                type="radio"
+                id={answerDecoded}
+                name={name}
+                onClick={props.handleClick}
+            />
+            </label>
+        );
+        });
 
-    let correctAnswerCount = 0
-    
+        return (
+        <div key={nanoid()} className="questions--questionComponent">
+            {question}
+            <div className="questions--answerContainer">{answers}</div>
+        </div>
+        );
+    });
+
+    const correctAnswers = props.data.map((item) => item.correctAnswer);
+
+    let correctAnswerCount = 0;
+
     correctAnswers.map((answer, index) => {
-        return (answer === props.userAnswers[`question${index + 1}`] ? correctAnswerCount++ : null)
-    })
+        return answer === props.userAnswers[`question${index + 1}`]
+        ? correctAnswerCount++
+        : null;
+    });
 
-    const result = <div className="result">You scored {correctAnswerCount}/5 correct answers</div>
-    
+    const result = (
+        <div className="result">
+        You scored {correctAnswerCount}/5 correct answers
+        </div>
+    );
+
     return (
         <div className="questions--body">
-            {questionComponent}
-            <div className="result--container">
-                {props.quizFinished && result}
-                <button className="button" onClick={props.quizFinished ? props.restartQuiz : checkAnswers}>
-                    {props.quizFinished ? "Play Again" : "Check Answers"}
-                </button>
-            </div>
+        {questionComponent}
+        <div className="result--container">
+            {props.quizFinished && result}
+            <button
+            className="button"
+            onClick={props.quizFinished ? props.restartQuiz : checkAnswers}
+            >
+            {props.quizFinished ? 'Play Again' : 'Check Answers'}
+            </button>
         </div>
-    )
+        </div>
+    );
 }
